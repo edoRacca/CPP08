c76abb9b729b6503f800de863286869b5e669c54
Edoardo Raccanello <edraccan@c1r4p6.42firenze.it>
2025-09-05 17:40:38 +0200

    ex00

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,15 @@
+
+#ifndef EASYFIND_H
+# define EASYFIND_H
+
+# include <iostream>
+# include <algorithm>
+# include <vector>
+# include <iterator>
+
+template <typename T> int easyfind(T& t, int val)
+{
+	
+}
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,12 @@
+
+// #include "easyfind.hpp"
+#include <iostream>
+#include <vector>
+#include <list>
+
+
+int main()
+{
+	std::vector<int> i;
+	i.pop_back();
+}
