a0aa6d69f8530ceb2ff66a471e73dbf4a53a7ffa
Edoardo Raccanello <edraccan@c1r4p3.42firenze.it>
2025-09-08 17:14:38 +0200

    ex00 done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -9,7 +9,10 @@
 
 template <typename T> int easyfind(T& t, int val)
 {
-	
-}
+	typename T::iterator iter = std::find(t.begin(), t.end(), val);
+	if (iter == t.end())
+		return (-1);
+	return (*iter);
+};
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,5 +1,5 @@
 
-// #include "easyfind.hpp"
+#include "easyfind.hpp"
 #include <iostream>
 #include <vector>
 #include <list>
@@ -7,6 +7,14 @@
 
 int main()
 {
-	std::vector<int> i;
-	i.pop_back();
+	std::vector<int> v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+
+	int val1 = 12;
+	int val2 = 10;
+
+	std::cout << "Valore " << val1 << (easyfind(v, val1) != -1 ? " trovato " : " non trovato") << std::endl;
+	std::cout << "Valore " << val2 << (easyfind(v, val2) != -1 ? " trovato " : " non trovato") << std::endl;
 }
