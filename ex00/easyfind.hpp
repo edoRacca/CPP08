60dcc38510efaa95565e0f8b59e47408dca98894
edoRacca <edoraccanello@gmail.com>
2025-09-09 11:03:38 +0200

    changed ex00 and finished

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -3,16 +3,20 @@
 # define EASYFIND_H
 
 # include <iostream>
+# include <exception>
 # include <algorithm>
 # include <vector>
 # include <iterator>
 
-template <typename T> int easyfind(T& t, int val)
+# define RED "\e[0;31m"
+# define WHITE "\e[0m"
+
+template <typename T> typename T::iterator easyfind(T& t, int val)
 {
 	typename T::iterator iter = std::find(t.begin(), t.end(), val);
 	if (iter == t.end())
-		return (-1);
-	return (*iter);
+		throw (std::runtime_error("Value not found"));
+	return (iter);
 };
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,17 +4,28 @@
 #include <vector>
 #include <list>
 
+template <typename T> void test(T& t, int val)
+{
+	try
+	{
+		std::cout << *(easyfind(t, val)) << std::endl;
+		std::cout << *(++easyfind(t, val)) << std::endl;
+	}
+	catch(const std::runtime_error& e)
+	{
+		std::cerr << RED"" << e.what() << WHITE"" << '\n';
+	}
+}
 
 int main()
 {
 	std::vector<int> v;
 	v.push_back(10);
 	v.push_back(20);
+	v.push_back(10);
 	v.push_back(30);
 
-	int val1 = 12;
-	int val2 = 10;
+	test(v, 12);
+	test(v, 10);	
 
-	std::cout << "Valore " << val1 << (easyfind(v, val1) != -1 ? " trovato " : " non trovato") << std::endl;
-	std::cout << "Valore " << val2 << (easyfind(v, val2) != -1 ? " trovato " : " non trovato") << std::endl;
 }
